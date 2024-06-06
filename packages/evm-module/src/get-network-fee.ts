f3191626c9141a04b619f37906ddea0583b4b573
neven-s <piaf1986@gmail.com>
2024-06-06 16:28:47 +0200

    Remove presetMultipliers

--- packages/evm-module/src/get-network-fee.ts
+++ packages/evm-module/src/get-network-fee.ts
@@ -1,7 +1,5 @@
 import { JsonRpcProvider } from 'ethers';
-import type { NetworkFees, Wei } from '@internal/types';
-
-type PresetKeys = 'LOW' | 'MEDIUM' | 'HIGH';
+import type { NetworkFees } from '@internal/types';
 
 const DEFAULT_PRESETS = {
   LOW: 1n,
@@ -11,18 +9,15 @@ const DEFAULT_PRESETS = {
 
 const BASE_PRIORITY_FEE_WEI = 500000000n; //0.5 GWei
 
-export async function getNetworkFee(
-  provider: JsonRpcProvider,
-  presetMultipliers: { [T in PresetKeys]: Wei } = DEFAULT_PRESETS,
-): Promise<NetworkFees | undefined> {
+export async function getNetworkFee(provider: JsonRpcProvider): Promise<NetworkFees | undefined> {
   const { maxFeePerGas: maxFeePerGasInWei } = await provider.getFeeData();
   if (!maxFeePerGasInWei) {
     return undefined;
   }
 
-  const lowMaxTip = BASE_PRIORITY_FEE_WEI * presetMultipliers.LOW;
-  const mediumMaxTip = BASE_PRIORITY_FEE_WEI * presetMultipliers.MEDIUM;
-  const highMaxTip = BASE_PRIORITY_FEE_WEI * presetMultipliers.HIGH;
+  const lowMaxTip = BASE_PRIORITY_FEE_WEI * DEFAULT_PRESETS.LOW;
+  const mediumMaxTip = BASE_PRIORITY_FEE_WEI * DEFAULT_PRESETS.MEDIUM;
+  const highMaxTip = BASE_PRIORITY_FEE_WEI * DEFAULT_PRESETS.HIGH;
   return {
     baseFee: maxFeePerGasInWei,
     low: {
