3a03ae21917968352beb0c292d860061a6a0e22b
ruijialin <ruijia.lin@avalabs.org>
2024-07-03 09:06:36 -0400

    add avalanche module

---
+++ packages/avalanche-module/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '.ts': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
+  },
+};
