16e298b934362196fd1cb0c97a8f3b105bddc92b
An Nguyen <an.nguyen@avalabs.org>
2024-07-11 10:16:14 -0500

    test universal ui

---
+++ packages/evm-module/src/universal-ui.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'tamagui';
+
+export const renderUniversalText = () => {
+  return <Text style={{ alignSelf: 'center', color: 'white', marginVertical: 10 }}>Universal Text</Text>;
+};
