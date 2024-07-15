572302d8a6647838be30acc72e7ccff193f95930
ruijialin <ruijia.lin@avalabs.org>
2024-07-15 09:51:33 -0400

    make maxPriorityFeePerGas optional

--- packages/types/src/network-fee.ts
+++ packages/types/src/network-fee.ts
@@ -1,7 +1,7 @@
 export type NetworkFees = {
   low: { maxFeePerGas: bigint; maxPriorityFeePerGas?: bigint };
-  medium: { maxFeePerGas: bigint; maxPriorityFeePerGas: bigint };
-  high: { maxFeePerGas: bigint; maxPriorityFeePerGas: bigint };
+  medium: { maxFeePerGas: bigint; maxPriorityFeePerGas?: bigint };
+  high: { maxFeePerGas: bigint; maxPriorityFeePerGas?: bigint };
   baseFee: bigint;
   isFixedFee: boolean;
 };
