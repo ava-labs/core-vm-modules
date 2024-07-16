62d139524f0532d435229c32039b8613732c3542
ruijialin <ruijia.lin@avalabs.org>
2024-07-16 17:15:50 -0400

    update package.json

--- packages-internal/utils/tsup.config.ts
+++
@@ -1,4 +0,0 @@
-import { defineConfig } from 'tsup';
-import { baseConfig } from '@internal/tsup-config';
-
-export default defineConfig({ ...baseConfig, entry: ['src/index.ts', 'src/utils/**/*.ts', 'src/services/**/*.ts'] });
