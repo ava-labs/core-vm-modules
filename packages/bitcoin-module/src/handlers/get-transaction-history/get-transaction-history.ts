e22c596b6f277f92305aa2124ba17044880bdf47
Michał Leszczyk <michal.leszczyk@avalabs.org>
2024-07-23 09:57:26 +0200

    feat(bitcoin-module): implement getTransactionHistory (#81)
    * refactor: move env.ts to shared utils
    
    * refactor: move getEnv() back from @internal/utils
    
    * feat(bitcoin-module): implement getTransactionHistory

---
+++ packages/bitcoin-module/src/handlers/get-transaction-history/get-transaction-history.ts
@@ -0,0 +1,23 @@
+import type { Network } from '@avalabs/vm-module-types';
+
+import { getProvider } from '../../utils/get-provider';
+
+import { convertBtcTransaction } from './convert-btc-transaction';
+
+type GetBtcTransactionHistoryOptions = {
+  network: Network;
+  address: string;
+  proxyApiUrl: string;
+};
+
+export const getTransactionHistory = async ({ address, network, proxyApiUrl }: GetBtcTransactionHistoryOptions) => {
+  const provider = getProvider({ isTestnet: Boolean(network.isTestnet), proxyApiUrl });
+  const rawHistory = await provider.getTxHistory(address);
+
+  return rawHistory.map((tx) =>
+    convertBtcTransaction(tx, {
+      address,
+      network,
+    }),
+  );
+};
