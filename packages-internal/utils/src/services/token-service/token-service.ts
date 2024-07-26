382ae28f955d0abaad317f66e86ee6af750bb03c
ruijialin <ruijia.lin@avalabs.org>
2024-07-26 14:08:59 -0400

    add missing currencies param to transformSimplePriceResponse

--- packages-internal/utils/src/services/token-service/token-service.ts
+++ packages-internal/utils/src/services/token-service/token-service.ts
@@ -152,7 +152,7 @@ export class TokenService {
           include_last_updated_at: String(lastUpdated),
         },
       });
-      return this.transformSimplePriceResponse(rawData);
+      return this.transformSimplePriceResponse(rawData, currencies);
     }
     return simplePrice(coingeckoBasicClient, {
       coinIds,
