dec087cc7c88f575f795ad6068a722b9c36542db
Viktor Vasas <viktor.vasas@avalabs.org>
2024-08-23 16:34:45 +0200

    chore: modify build config for provider

--- packages-internal/tsup-config/index.js
+++ packages-internal/tsup-config/index.js
@@ -5,7 +5,7 @@
  */
 export const baseConfig = {
   format: ['cjs', 'esm'],
-  entry: ['./src/index.ts', './src/provider.ts'],
+  entry: ['./src/index.ts'],
   sourcemap: true,
   clean: true,
   dts: true,
