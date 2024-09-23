4c011719292c40bab59da17bf4c812432e8063c9
ruijialin <ruijia.lin@avalabs.org>
2024-09-23 14:00:25 -0400

    update function name

---
+++ packages-internal/utils/src/utils/get-core-headers.ts
@@ -0,0 +1,23 @@
+import { AppName } from '@avalabs/vm-module-types';
+
+export const getCoreHeaders = ({
+  appName,
+  appVersion,
+}: {
+  appName: AppName;
+  appVersion: string;
+}): Record<string, string> | undefined => {
+  switch (appName) {
+    case AppName.CORE_MOBILE_IOS:
+    case AppName.CORE_MOBILE_ANDROID:
+    case AppName.CORE_WEB:
+    case AppName.CORE_EXTENSION:
+    case AppName.EXPLORER:
+      return {
+        'x-application-name': appName,
+        'x-application-version': appVersion,
+      };
+    case AppName.OTHER:
+      return undefined;
+  }
+};
