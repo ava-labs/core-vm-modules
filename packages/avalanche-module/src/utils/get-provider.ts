263f7657090e05df2f1e95d22510af7856e716b7
ruijialin <ruijia.lin@avalabs.org>
2024-10-17 14:51:52 -0400

    set etna-url

--- packages/avalanche-module/src/utils/get-provider.ts
+++ packages/avalanche-module/src/utils/get-provider.ts
@@ -5,9 +5,12 @@ import { Avalanche } from '@avalabs/core-wallets-sdk';
 type ProviderParams = {
   isTestnet: boolean;
 };
+export const AVALANCHE_TESTNET_API_URL = 'https://etna.avax-dev.network';
 
 export const getProvider = async ({ isTestnet }: ProviderParams): Promise<Avalanche.JsonRpcProvider> => {
-  const network = isTestnet ? AVALANCHE_XP_TEST_NETWORK : AVALANCHE_XP_NETWORK;
+  const network = isTestnet
+    ? { ...AVALANCHE_XP_TEST_NETWORK, rpcUrl: AVALANCHE_TESTNET_API_URL }
+    : AVALANCHE_XP_NETWORK;
   const upgradesInfo = await new Info(network.rpcUrl)
     .getUpgradesInfo()
     .then((info) => info)
