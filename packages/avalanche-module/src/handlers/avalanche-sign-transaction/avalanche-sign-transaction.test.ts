9c818f1efc876ac9841589e852f7b6c3fd639b8e
Michał Leszczyk <michal.leszczyk@avalabs.org>
2024-10-31 16:15:29 +0100

    chore: update failing tests

--- packages/avalanche-module/src/handlers/avalanche-sign-transaction/avalanche-sign-transaction.test.ts
+++ packages/avalanche-module/src/handlers/avalanche-sign-transaction/avalanche-sign-transaction.test.ts
@@ -3,6 +3,7 @@ import { AppName, NetworkVMType, RpcMethod, TxType } from '@avalabs/vm-module-ty
 import { Avalanche } from '@avalabs/core-wallets-sdk';
 import { avalancheSignTransaction } from './avalanche-sign-transaction';
 import { rpcErrors } from '@metamask/rpc-errors';
+import type { GetUpgradesInfoResponse } from '@avalabs/avalanchejs/dist/info/model';
 
 jest.mock('@avalabs/avalanchejs');
 jest.mock('@avalabs/core-wallets-sdk');
@@ -74,6 +75,8 @@ describe('avalanche-sign-transaction', () => {
   };
   beforeEach(() => {
     jest.resetAllMocks();
+
+    jest.spyOn(Info.prototype, 'getUpgradesInfo').mockResolvedValue({} as GetUpgradesInfoResponse);
     (UnsignedTx.fromJSON as jest.Mock).mockReturnValue(unsignedTxMock);
     (Avalanche.getVmByChainAlias as jest.Mock).mockReturnValue(PVM);
     (Avalanche.createAvalancheUnsignedTx as jest.Mock).mockReturnValue(unsignedTxMock);
--- packages/avalanche-module/src/handlers/get-network-fee/get-network-fee.test.ts
+++ packages/avalanche-module/src/handlers/get-network-fee/get-network-fee.test.ts
@@ -1,8 +1,9 @@
+import { NetworkVMType } from '@avalabs/vm-module-types';
 import { getNetworkFee } from './get-network-fee';
 
 describe('get-network-fee', () => {
-  it('should return fixed network fees', async () => {
-    await expect(getNetworkFee()).resolves.toEqual({
+  it('should return fixed network fees for X-Chain', async () => {
+    await expect(getNetworkFee({ isTestnet: false, vmName: NetworkVMType.AVM })).resolves.toEqual({
       baseFee: 1000000n,
       low: {
         maxFeePerGas: 1000000n,
