eb8484f3f6a3fde67d430ce82eb0c363989aaacf
ruijialin <ruijia.lin@avalabs.org>
2024-12-06 10:49:55 -0500

    update AvalancheChainStrings

--- packages/avalanche-module/src/constants.ts
+++ packages/avalanche-module/src/constants.ts
@@ -1,7 +1,7 @@
 export const AVAX_NONEVM_DENOMINATION = 9;
 
 export enum AvalancheChainStrings {
-  AVM = 'X Chain',
-  PVM = 'P Chain',
-  EVM = 'C Chain',
+  AVM = 'X-Chain',
+  PVM = 'P-Chain',
+  EVM = 'C-Chain',
 }
