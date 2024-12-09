2df5e36958fbc033122fad4747fbde8af6b2f2d5
ruijialin <ruijia.lin@avalabs.org>
2024-12-09 14:06:03 -0500

    revert to if else logic

--- packages/avalanche-module/src/utils/get-transaction-detail-sections.ts
+++ packages/avalanche-module/src/utils/get-transaction-detail-sections.ts
@@ -1,4 +1,4 @@
-import { TxType, type DetailSection, type TxDetails } from '@avalabs/vm-module-types';
+import { type TxDetails } from '@avalabs/vm-module-types';
 import {
   isAddPermissionlessDelegatorTx,
   isAddPermissionlessValidatorTx,
@@ -32,77 +32,34 @@ import {
   setL1ValidatorWeightDetailSection,
 } from './transaction-detail-sections';
 
-type TxHandlerMapping = {
-  [key in TxType]?: {
-    typeGuard: (tx: TxDetails) => boolean; // The type guard function
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    handler: (tx: any, symbol: string) => DetailSection[];
-  };
-};
-
-const txHandlerMapping: TxHandlerMapping = {
-  [TxType.Base]: {
-    typeGuard: isChainDetails,
-    handler: chainDetailSection,
-  },
-  [TxType.AddPermissionlessDelegator]: {
-    typeGuard: isAddPermissionlessDelegatorTx,
-    handler: addPermissionlessDelegatorDetailSection,
-  },
-  [TxType.AddPermissionlessValidator]: {
-    typeGuard: isAddPermissionlessValidatorTx,
-    handler: addPermissionlessValidatorDetailSection,
-  },
-  [TxType.Export]: {
-    typeGuard: isExportTx,
-    handler: exportDetailSection,
-  },
-  [TxType.Import]: {
-    typeGuard: isImportTx,
-    handler: importDetailSection,
-  },
-  [TxType.AddSubnetValidator]: {
-    typeGuard: isAddSubnetValidatorTx,
-    handler: addSubnetValidatorDetailSection,
-  },
-  [TxType.RemoveSubnetValidator]: {
-    typeGuard: isRemoveSubnetValidatorTx,
-    handler: removeSubnetValidatorDetailSection,
-  },
-  [TxType.ConvertSubnetToL1]: {
-    typeGuard: isConvertSubnetToL1Tx,
-    handler: convertSubnetToL1DetailSection,
-  },
-  [TxType.DisableL1Validator]: {
-    typeGuard: isDisableL1ValidatorTx,
-    handler: disableL1ValidatorDetailSection,
-  },
-  [TxType.IncreaseL1ValidatorBalance]: {
-    typeGuard: isIncreaseL1ValidatorBalanceTx,
-    handler: increaseL1ValidatorBalanceDetailSection,
-  },
-  [TxType.RegisterL1Validator]: {
-    typeGuard: isRegisterL1ValidatorTx,
-    handler: registerL1ValidatorDetailSection,
-  },
-  [TxType.SetL1ValidatorWeight]: {
-    typeGuard: isSetL1ValidatorWeightTx,
-    handler: setL1ValidatorWeightDetailSection,
-  },
-  [TxType.CreateChain]: {
-    typeGuard: isBlockchainDetails,
-    handler: blockChainDetailSection,
-  },
-  [TxType.CreateSubnet]: {
-    typeGuard: isSubnetDetails,
-    handler: subnetDetailSection,
-  },
-};
-
 export const getTransactionDetailSections = (txDetails: TxDetails, symbol: string) => {
-  const mapping = txHandlerMapping[txDetails.type];
-  if (mapping && mapping.typeGuard(txDetails)) {
-    return mapping.handler(txDetails, symbol);
+  if (isChainDetails(txDetails)) {
+    return chainDetailSection(txDetails, symbol);
+  } else if (isExportTx(txDetails)) {
+    return exportDetailSection(txDetails, symbol);
+  } else if (isImportTx(txDetails)) {
+    return importDetailSection(txDetails, symbol);
+  } else if (isSubnetDetails(txDetails)) {
+    return subnetDetailSection(txDetails, symbol);
+  } else if (isAddPermissionlessDelegatorTx(txDetails)) {
+    return addPermissionlessDelegatorDetailSection(txDetails, symbol);
+  } else if (isAddPermissionlessValidatorTx(txDetails)) {
+    return addPermissionlessValidatorDetailSection(txDetails, symbol);
+  } else if (isBlockchainDetails(txDetails)) {
+    return blockChainDetailSection(txDetails, symbol);
+  } else if (isAddSubnetValidatorTx(txDetails)) {
+    return addSubnetValidatorDetailSection(txDetails, symbol);
+  } else if (isRemoveSubnetValidatorTx(txDetails)) {
+    return removeSubnetValidatorDetailSection(txDetails, symbol);
+  } else if (isConvertSubnetToL1Tx(txDetails)) {
+    return convertSubnetToL1DetailSection(txDetails, symbol);
+  } else if (isDisableL1ValidatorTx(txDetails)) {
+    return disableL1ValidatorDetailSection(txDetails, symbol);
+  } else if (isIncreaseL1ValidatorBalanceTx(txDetails)) {
+    return increaseL1ValidatorBalanceDetailSection(txDetails, symbol);
+  } else if (isRegisterL1ValidatorTx(txDetails)) {
+    return registerL1ValidatorDetailSection(txDetails, symbol);
+  } else if (isSetL1ValidatorWeightTx(txDetails)) {
+    return setL1ValidatorWeightDetailSection(txDetails, symbol);
   }
-  throw new Error(`Unsupported or invalid transaction type: ${txDetails.type}`);
 };
