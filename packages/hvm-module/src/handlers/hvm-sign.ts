0061beba871c10eaae999aaf415914189fce8d64
Viktor Vasas <viktor.vasas@avalabs.org>
2025-01-02 20:03:23 +0100

    feat: add basic hvm sign display data

--- packages/hvm-module/src/handlers/hvm-sign.ts
+++ packages/hvm-module/src/handlers/hvm-sign.ts
@@ -11,11 +11,24 @@ export const hvmSign = async ({
   approvalController: ApprovalController;
 }) => {
   // eslint-disable-next-line no-console
-  console.log('network: ', network);
-  // eslint-disable-next-line no-console
   console.log('hvmSignMessage: ');
   // eslint-disable-next-line no-console
-  const displayData = {} as unknown as DisplayData;
+  console.log('network: ', network);
+  // eslint-disable-next-line no-console
+  console.log('request: ', request);
+  const displayData = {
+    title: 'Sign Message',
+    dAppInfo: {
+      name: request.dappInfo.name,
+      action: `${request.dappInfo.name} requests you to sign the following message`,
+      logoUri: request.dappInfo.icon,
+    },
+    network: {
+      chainId: network.chainId,
+      name: network.chainName,
+      logoUri: network.logoUri,
+    },
+  } as unknown as DisplayData;
   const signingData = {} as unknown as SigningData;
   const response = await approvalController.requestApproval({ request, displayData, signingData });
   if ('error' in response) {
