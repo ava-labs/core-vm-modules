5e48c48b38850924b47c339b6800559ba8fa0d99
Viktor Vasas <viktor.vasas@avalabs.org>
2025-01-06 12:33:17 +0100

    chore: remove log

--- packages/hvm-module/src/handlers/hvm-get-balances.ts
+++ packages/hvm-module/src/handlers/hvm-get-balances.ts
@@ -2,15 +2,12 @@ import { type HvmGetBalanceParams, TokenType } from '@avalabs/vm-module-types';
 import { TokenUnit } from '@avalabs/core-utils-sdk';
 
 export const hvmGetBalances = async (params: HvmGetBalanceParams) => {
-  const { sdkClient, addresses, currency, network } = params;
+  const { sdkClient, addresses, network } = params;
   if (!addresses[0]) {
     throw new Error('no address');
   }
   const balanceResult = await sdkClient.getBalance(addresses[0]);
 
-  const lowercaseCurrency = currency.toLowerCase();
-  // eslint-disable-next-line no-console
-  console.log('lowercaseCurrency: ', lowercaseCurrency);
   const address = addresses[0] ?? '';
   const networkToken = network.networkToken;
   const totalBalance = new TokenUnit(balanceResult, networkToken.decimals, networkToken.symbol);
