7bee31467724585b64e0fc148dfa285a897bb2f8
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-01-24 19:10:27 +0100

    feat: create an empty Solana module (#216)
    * feat: create an empty Solana module
    
    * docs(changeset): feat: adds an empty module for Solana VM
    
    * fix: global pnpm-lock.yaml

--- packages/types/src/module.ts
+++ packages/types/src/module.ts
@@ -9,6 +9,7 @@ import type { NetworkContractToken } from './token';
 import type { GetTransactionHistory, TransactionHistoryResponse } from './transaction-history';
 import type { ApprovalController } from './rpc';
 import type { HyperSDKClient } from 'hypersdk-client';
+import { type Rpc, type SolanaRpcApiDevnet, type SolanaRpcApiMainnet } from '@solana/rpc';
 
 export type AppInfo = {
   name: AppName;
@@ -26,7 +27,13 @@ export type NetworkFeeParam = Network & { caipId?: string };
 export interface Module {
   getProvider: (
     network: Network,
-  ) => Promise<JsonRpcBatchInternal | BitcoinProvider | Avalanche.JsonRpcProvider | HyperSDKClient>;
+  ) => Promise<
+    | JsonRpcBatchInternal
+    | BitcoinProvider
+    | Avalanche.JsonRpcProvider
+    | HyperSDKClient
+    | Rpc<SolanaRpcApiMainnet | SolanaRpcApiDevnet>
+  >;
   getManifest: () => Manifest | undefined;
   getBalances: (params: GetBalancesParams) => Promise<GetBalancesResponse>;
   getTransactionHistory: (params: GetTransactionHistory) => Promise<TransactionHistoryResponse>;
