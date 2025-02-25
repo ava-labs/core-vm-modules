75b527a7576bcd0843daefc1b424610f2fade938
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-02-25 14:29:43 +0100

    fix: types

--- packages/svm-module/src/utils/get-provider.ts
+++ packages/svm-module/src/utils/get-provider.ts
@@ -1,8 +1,14 @@
-import { getSolanaProvider } from '@avalabs/core-wallets-sdk';
+import { getSolanaProvider, type SolanaProvider } from '@avalabs/core-wallets-sdk';
 
 import { RPC_URL_DEVNET, RPC_URL_PROXY_API_ENDPOINT } from '../constants';
 
-export const getProvider = ({ isTestnet, proxyApiUrl }: { isTestnet: boolean; proxyApiUrl: string }) => {
+export const getProvider = ({
+  isTestnet,
+  proxyApiUrl,
+}: {
+  isTestnet: boolean;
+  proxyApiUrl: string;
+}): SolanaProvider => {
   return getSolanaProvider({
     isTestnet,
     rpcUrl: isTestnet ? RPC_URL_DEVNET : proxyApiUrl + RPC_URL_PROXY_API_ENDPOINT,
