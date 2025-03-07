21ed5c89e21b2fc970eb09fa25a59aa0fcbd5086
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-03-07 11:23:20 +0100

    fix: typings

--- packages/avalanche-module/src/utils/get-provider.ts
+++ packages/avalanche-module/src/utils/get-provider.ts
@@ -1,5 +1,3 @@
-import { info } from '@avalabs/avalanchejs';
-import { AVALANCHE_XP_NETWORK, AVALANCHE_XP_TEST_NETWORK } from '@avalabs/core-chains-sdk';
 import { Avalanche } from '@avalabs/core-wallets-sdk';
 
 type ProviderParams = {
@@ -7,10 +5,7 @@ type ProviderParams = {
 };
 
 export const getProvider = async ({ isTestnet }: ProviderParams): Promise<Avalanche.JsonRpcProvider> => {
-  const network = isTestnet ? AVALANCHE_XP_TEST_NETWORK : AVALANCHE_XP_NETWORK;
-  const upgradesInfo = await new info.InfoApi(network.rpcUrl).getUpgradesInfo().catch(() => undefined);
-
   return isTestnet
-    ? Avalanche.JsonRpcProvider.getDefaultFujiProvider(upgradesInfo)
-    : Avalanche.JsonRpcProvider.getDefaultMainnetProvider(upgradesInfo);
+    ? Avalanche.JsonRpcProvider.getDefaultFujiProvider()
+    : Avalanche.JsonRpcProvider.getDefaultMainnetProvider();
 };
