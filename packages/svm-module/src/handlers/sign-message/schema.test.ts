bd89248af5d192101548d12c5490f552e5d5822e
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-04-04 10:40:48 +0200

    fix: make sure passed message is not a tx

--- packages/svm-module/src/handlers/sign-message/schema.test.ts
+++ packages/svm-module/src/handlers/sign-message/schema.test.ts
@@ -2,7 +2,7 @@ import { parseRequestParams } from './schema';
 
 describe('src/handlers/sign-message/schema', () => {
   it('successfully parses valid params', () => {
-    const validParams = [{ account: 'testAccount', serializedMessage: 'dGVzdFR4' }];
+    const validParams = [{ account: 'testAccount', serializedMessage: 'SGVsbG8gdGhlcmU=' }]; // "Hello there" in base64
     const result = parseRequestParams(validParams);
     expect(result.success).toBe(true);
     if (result.success) {
@@ -20,5 +20,18 @@ describe('src/handlers/sign-message/schema', () => {
     const missingFieldsParams = [{ account: 'testAccount' }];
     const result = parseRequestParams(missingFieldsParams);
     expect(result.success).toBe(false);
+  });
+
+  it('forbids passing transaction bytes', () => {
+    // This would send 0.001 SOL
+    const encodedTxBytes =
+      'gAEAAQNPAVKo0doHY8oS/vZ018d8Fd7uABsPsU4d9a8tA3Hb0QqEpfYzPGQw9mpl1Xxbk6uIEt604LAGkj/XmZyITucxAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAsdqW9XERUw+73+mjQFregnod5ljulod+8/5B1hbHJdgECAgABDAIAAACAlpgAAAAAAAA=';
+    const transactionBytesParams = [{ account: 'testAccount', serializedMessage: encodedTxBytes }];
+
+    const result = parseRequestParams(transactionBytesParams);
+    expect(result.success).toBe(false);
+    expect(result.error?.issues[0]).toEqual(
+      expect.objectContaining({ message: 'Cannot use signMessage() calls for signing transactions' }),
+    );
   });
 });
