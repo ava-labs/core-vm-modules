4a6f6e0c97188403080d6ab1e9f8070df3259f74
An Nguyen <an.nguyen@avalabs.org>
2025-04-24 18:45:13 -0500

    fix test

--- packages/bitcoin-module/src/utils/get-provider.test.ts
+++ packages/bitcoin-module/src/utils/get-provider.test.ts
@@ -26,12 +26,13 @@ describe('get-provider', () => {
     expect(result).toBeInstanceOf(BitcoinProvider);
   });
 
-  describe('when process.env.GLACIER_API_KEY is defined', () => {
+  describe('when process.env.GLACIER_API_KEY is defined and process.env.NODE_ENV is development', () => {
     const env = process.env;
 
     beforeAll(() => {
       process.env = {
         ...env,
+        NODE_ENV: 'development',
         GLACIER_API_KEY: 'glacier-api-key',
       };
     });
