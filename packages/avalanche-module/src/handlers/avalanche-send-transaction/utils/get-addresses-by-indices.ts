a48d1b15ae2b8007fc1b4dcdd8e5e153ab295777
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-11-14 19:10:08 +0100

    fix: allow passing XP addresses in context (#340)
    * feat: pass all known XP addresses
    
    * docs(changeset): feat(avalanche-module): pass xp account info
    
    * fix: typings
    
    * test(avalanche-module): make sure the legacy context schema still works

--- packages/avalanche-module/src/handlers/avalanche-send-transaction/utils/get-addresses-by-indices.ts
+++ packages/avalanche-module/src/handlers/avalanche-send-transaction/utils/get-addresses-by-indices.ts
@@ -7,18 +7,32 @@ export const getAddressesByIndices = async ({
   isChange,
   isTestnet,
   xpubXP,
+  xpAddresses,
 }: {
   indices: number[];
   chainAlias: 'X' | 'P';
   isChange: boolean;
   isTestnet: boolean;
   xpubXP?: string;
+  xpAddresses?: { index: number; address: string }[];
 }): Promise<string[]> => {
-  if (!xpubXP || (isChange && chainAlias !== 'X')) {
+  if (isChange && chainAlias !== 'X') {
     return [];
   }
 
-  const provider = await getProvider({ isTestnet });
+  if (xpubXP) {
+    const provider = await getProvider({ isTestnet });
 
-  return indices.map((index) => Avalanche.getAddressFromXpub(xpubXP, index, provider, chainAlias, isChange));
+    return indices.map((index) => Avalanche.getAddressFromXpub(xpubXP, index, provider, chainAlias, isChange));
+  }
+
+  if (xpAddresses && Array.isArray(xpAddresses)) {
+    const accounts = indices
+      .map((index) => xpAddresses.find((address) => address.index === index))
+      .filter((acc) => acc !== undefined);
+
+    return accounts.map((account) => account.address);
+  }
+
+  return [];
 };
