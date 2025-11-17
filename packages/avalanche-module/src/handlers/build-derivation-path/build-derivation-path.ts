271fa94fb37937b52f30e38c57bd07d2576a251e
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-11-17 13:34:14 +0100

    refactor(avalanche): update derivation paths

--- packages/avalanche-module/src/handlers/build-derivation-path/build-derivation-path.ts
+++ packages/avalanche-module/src/handlers/build-derivation-path/build-derivation-path.ts
@@ -7,26 +7,13 @@ import { rpcErrors } from '@metamask/rpc-errors';
 
 export const buildDerivationPath = ({
   accountIndex,
-  derivationPathType,
 }: BuildDerivationPathParams): Pick<BuildDerivationPathResponse, NetworkVMType.AVM | NetworkVMType.CoreEth> => {
   if (accountIndex < 0) {
     throw rpcErrors.invalidParams('Account index must be a non-negative integer');
   }
 
-  switch (derivationPathType) {
-    case 'bip44':
-      return {
-        [NetworkVMType.AVM]: `m/44'/9000'/0'/0/${accountIndex}`,
-        [NetworkVMType.CoreEth]: `m/44'/60'/0'/0/${accountIndex}`,
-      };
-
-    case 'ledger_live':
-      return {
-        [NetworkVMType.AVM]: `m/44'/9000'/${accountIndex}'/0/0`,
-        [NetworkVMType.CoreEth]: `m/44'/60'/${accountIndex}'/0/0`,
-      };
-
-    default:
-      throw rpcErrors.invalidParams(`Unsupported derivation path type: ${derivationPathType}`);
-  }
+  return {
+    [NetworkVMType.AVM]: `m/44'/9000'/${accountIndex}'/0/0`,
+    [NetworkVMType.CoreEth]: `m/44'/60'/${accountIndex}'/0/0`,
+  };
 };
