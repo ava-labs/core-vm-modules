9b9234686a1eba938a8fb1c9a424a0901a839a4d
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-11-19 10:11:05 +0100

    fix: core eth derivation path

--- packages/avalanche-module/src/handlers/build-derivation-path/build-derivation-path.ts
+++ packages/avalanche-module/src/handlers/build-derivation-path/build-derivation-path.ts
@@ -7,13 +7,26 @@ import { rpcErrors } from '@metamask/rpc-errors';
 
 export const buildDerivationPath = ({
   accountIndex,
+  derivationPathType,
 }: BuildDerivationPathParams): Pick<BuildDerivationPathResponse, NetworkVMType.AVM | NetworkVMType.CoreEth> => {
   if (accountIndex < 0) {
     throw rpcErrors.invalidParams('Account index must be a non-negative integer');
   }
 
-  return {
-    [NetworkVMType.AVM]: `m/44'/9000'/${accountIndex}'/0/0`,
-    [NetworkVMType.CoreEth]: `m/44'/60'/0'/0/${accountIndex}`,
-  };
+  switch (derivationPathType) {
+    case 'bip44':
+      return {
+        [NetworkVMType.AVM]: `m/44'/9000'/${accountIndex}'/0/0`,
+        [NetworkVMType.CoreEth]: `m/44'/60'/0'/0/${accountIndex}`, // Must match the derivation path used by the EMV Module
+      };
+
+    case 'ledger_live':
+      return {
+        [NetworkVMType.AVM]: `m/44'/9000'/${accountIndex}'/0/0`,
+        [NetworkVMType.CoreEth]: `m/44'/60'/${accountIndex}'/0/0`, // Must match the derivation path used by the EMV Module
+      };
+
+    default:
+      throw rpcErrors.invalidParams(`Unsupported derivation path type: ${derivationPathType}`);
+  }
 };
