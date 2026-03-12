87fb41574981d360afd655280295eb200afc300c
Fumiko <fumikom@gmail.com>
2026-03-12 14:40:58 -0600

    fix: support more networks for history

--- packages/evm-module/src/handlers/get-transaction-history/get-transaction-history.test.ts
+++ packages/evm-module/src/handlers/get-transaction-history/get-transaction-history.test.ts
@@ -1,5 +1,6 @@
 import { getTransactionHistory } from './get-transaction-history';
 import { getTransactionFromEtherscan } from './converters/etherscan-transaction-converter/get-transaction-from-etherscan';
+import { getTransactionsFromExplorerApi } from './converters/etherscan-transaction-converter/get-transactions-from-explorer-api';
 import { getTransactionsFromGlacier } from './converters/evm-transaction-converter/get-transactions-from-glacier';
 import type { EvmGlacierService } from '../../services/glacier-service/glacier-service';
 
@@ -9,6 +10,10 @@ jest.mock('./converters/evm-transaction-converter/get-transactions-from-glacier'
 
 jest.mock('./converters/etherscan-transaction-converter/get-transaction-from-etherscan', () => ({
   getTransactionFromEtherscan: jest.fn(),
+}));
+
+jest.mock('./converters/etherscan-transaction-converter/get-transactions-from-explorer-api', () => ({
+  getTransactionsFromExplorerApi: jest.fn(),
 }));
 
 describe('get-transaction-history', () => {
@@ -31,7 +36,65 @@ describe('get-transaction-history', () => {
     });
     expect(getTransactionFromEtherscan).toHaveBeenCalled();
   });
-  it('should have called getTransactionsFromGlacier', async () => {
+  it('should have called getTransactionsFromExplorerApi for Base (8453)', async () => {
+    await getTransactionHistory({
+      glacierService: {} as EvmGlacierService,
+      chainId: 8453,
+      isTestnet: false,
+      networkToken: {
+        name: 'ETH',
+        symbol: 'ETH',
+        decimals: 18,
+        description: 'Ether',
+        logoUri: 'logoUri',
+      },
+      explorerUrl: 'https://basescan.org',
+      address: 'address',
+      nextPageToken: 'nextPageToken',
+      offset: 1,
+    });
+    expect(getTransactionsFromExplorerApi).toHaveBeenCalled();
+    expect(getTransactionsFromGlacier).not.toHaveBeenCalled();
+    expect(getTransactionFromEtherscan).not.toHaveBeenCalled();
+  });
+
+  it('should have called getTransactionsFromExplorerApi for Arbitrum (42161)', async () => {
+    await getTransactionHistory({
+      glacierService: {} as EvmGlacierService,
+      chainId: 42161,
+      isTestnet: false,
+      networkToken: {
+        name: 'ETH',
+        symbol: 'ETH',
+        decimals: 18,
+        description: 'Ether',
+        logoUri: 'logoUri',
+      },
+      explorerUrl: 'https://arbiscan.io',
+      address: 'address',
+    });
+    expect(getTransactionsFromExplorerApi).toHaveBeenCalled();
+  });
+
+  it('should have called getTransactionsFromExplorerApi for Optimism (10)', async () => {
+    await getTransactionHistory({
+      glacierService: {} as EvmGlacierService,
+      chainId: 10,
+      isTestnet: false,
+      networkToken: {
+        name: 'ETH',
+        symbol: 'ETH',
+        decimals: 18,
+        description: 'Ether',
+        logoUri: 'logoUri',
+      },
+      explorerUrl: 'https://optimistic.etherscan.io',
+      address: 'address',
+    });
+    expect(getTransactionsFromExplorerApi).toHaveBeenCalled();
+  });
+
+  it('should have called getTransactionsFromGlacier for non-L2 chains', async () => {
     const mockGlacierService: EvmGlacierService = {
       ...expect.any(Object),
       isHealthy: jest.fn().mockReturnValue(true),
@@ -53,5 +116,6 @@ describe('get-transaction-history', () => {
       offset: 1,
     });
     expect(getTransactionsFromGlacier).toHaveBeenCalled();
+    expect(getTransactionsFromExplorerApi).not.toHaveBeenCalled();
   });
 });
--- packages/evm-module/src/handlers/get-transaction-history/get-transaction-history.ts
+++ packages/evm-module/src/handlers/get-transaction-history/get-transaction-history.ts
@@ -1,5 +1,7 @@
 import { getTransactionFromEtherscan } from './converters/etherscan-transaction-converter/get-transaction-from-etherscan';
+import { getTransactionsFromExplorerApi } from './converters/etherscan-transaction-converter/get-transactions-from-explorer-api';
 import { isEthereumChainId } from './utils/is-ethereum-chain-id';
+import { getExplorerApiUrl } from './utils/explorer-api-urls';
 import type { NetworkToken, TransactionHistoryResponse } from '@avalabs/vm-module-types';
 import { getTransactionsFromGlacier } from './converters/evm-transaction-converter/get-transactions-from-glacier';
 import type { EvmGlacierService } from '../../services/glacier-service/glacier-service';
@@ -26,6 +28,19 @@ export const getTransactionHistory = async ({
   if (isEthereumChainId(chainId)) {
     return getTransactionFromEtherscan({
       isTestnet,
+      networkToken,
+      explorerUrl,
+      chainId,
+      address,
+      nextPageToken,
+      offset,
+    });
+  }
+
+  const explorerApiUrl = getExplorerApiUrl(chainId);
+  if (explorerApiUrl) {
+    return getTransactionsFromExplorerApi({
+      explorerApiUrl,
       networkToken,
       explorerUrl,
       chainId,
