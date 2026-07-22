8308555b58610be375fc3fb192f15d3f47fca1ee
Michał Leszczyk <michal.leszczyk@avalabs.org>
2026-07-22 11:49:18 +0200

    feat: export parseEercTransaction util

--- packages/evm-module/src/index.ts
+++ packages/evm-module/src/index.ts
@@ -2,3 +2,4 @@ export * from './module';
 export * from './types';
 export * from './handlers/eth-sign/utils/typeguards';
 export * from './utils/id-promise';
+export * from './utils/parse-eerc-transaction';
