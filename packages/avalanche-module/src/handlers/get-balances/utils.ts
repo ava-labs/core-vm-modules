0ecf7dc46ccfc7b8bcbeb5915eb0733106a019de
Gergely Lovas <36536513+gergelylovas@users.noreply.github.com>
2026-08-19 19:42:57 +0200

    fix: make calculateAvaxTotalBalance robust after glacier-sdk update (#466)

--- packages/avalanche-module/src/handlers/get-balances/utils.ts
+++ packages/avalanche-module/src/handlers/get-balances/utils.ts
@@ -1,4 +1,10 @@
-import type { PChainBalance, XChainBalances } from '@avalabs/glacier-sdk';
+import type {
+  AggregatedAssetAmount,
+  PChainBalance,
+  PChainSharedAsset,
+  XChainBalances,
+  XChainSharedAssetBalance,
+} from '@avalabs/glacier-sdk';
 
 export const isPchainBalance = (balanceResult: PChainBalance | XChainBalances): balanceResult is PChainBalance => {
   return Object.keys(balanceResult).includes('unlockedUnstaked');
@@ -8,18 +14,48 @@ export const isXchainBalance = (balanceResult: PChainBalance | XChainBalances): 
   return Object.keys(balanceResult).includes('locked');
 };
 
-export function calculateAvaxTotalBalance(uxtos: PChainBalance | XChainBalances, avaxAssetId: string): bigint {
-  const sum = Object.values(uxtos).reduce(function (totalAcc, utxoList) {
-    const typeSum = utxoList.reduce(function (typeAcc, utxo) {
-      if (utxo.assetId !== avaxAssetId) {
-        return typeAcc;
-      }
-      const balanceToAdd = Number(utxo.amount);
-      return typeAcc + balanceToAdd;
-    }, 0);
+type BalanceAsset = AggregatedAssetAmount | PChainSharedAsset | XChainSharedAssetBalance;
 
-    return totalAcc + typeSum;
-  }, 0);
+const sumAvaxFromAssets = (assets: BalanceAsset[] | undefined, avaxAssetId: string): bigint => {
+  if (!assets?.length) {
+    return 0n;
+  }
 
-  return BigInt(sum);
+  return assets.reduce((total, asset) => {
+    if (asset.assetId !== avaxAssetId) {
+      return total;
+    }
+
+    return total + BigInt(asset.amount);
+  }, 0n);
+};
+
+export function calculateAvaxTotalBalance(balances: PChainBalance | XChainBalances, avaxAssetId: string): bigint {
+  if (isPchainBalance(balances)) {
+    const balanceArrays = [
+      ...(balances.unlockedUnstaked || []),
+      ...(balances.unlockedStaked || []),
+      ...(balances.pendingStaked || []),
+      ...(balances.lockedStaked || []),
+      ...(balances.lockedStakeable || []),
+      ...(balances.lockedPlatform || []),
+      ...(balances.atomicMemoryLocked || []),
+      ...(balances.atomicMemoryUnlocked || []),
+    ];
+
+    const arrayTotal = sumAvaxFromAssets(balanceArrays, avaxAssetId);
+
+    const restakedRewards = balances.restakedRewards ? BigInt(balances.restakedRewards) : 0n;
+
+    return arrayTotal + restakedRewards;
+  }
+
+  const balanceArrays = [
+    ...(balances.unlocked || []),
+    ...(balances.locked || []),
+    ...(balances.atomicMemoryUnlocked || []),
+    ...(balances.atomicMemoryLocked || []),
+  ];
+
+  return sumAvaxFromAssets(balanceArrays, avaxAssetId);
 }
